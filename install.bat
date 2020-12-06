24a125320c4cf98e8ea1bf957a3ede29d73f6349
roy4801 <a82611141@gmail.com>
2020-12-06 18:48:10 +0800

    Update

--- install.bat
+++ install.bat
@@ -1,2 +1,2 @@
 REM Change the path to csgo\cfg
-copy *.cfg "D:\SteamLibrary\steamapps\common\Counter-Strike Global Offensive\csgo\cfg"
+copy *.cfg "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\cfg"
