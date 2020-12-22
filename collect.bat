d518e1ec197aa83d800f6bb6f2549ca6af6e0dea
roy4801 <a82611141@gmail.com>
2020-12-22 17:57:42 +0800

    Update

---
+++ collect.bat
@@ -0,0 +1,4 @@
+copy "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\cfg\autoexec.cfg" .
+copy "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\cfg\prac.cfg" .
+copy "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\cfg\throw.cfg" .
+copy "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\cfg\ze.cfg" .
